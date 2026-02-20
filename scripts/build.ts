732f00584704350e6d652421fd1ed33c600b4ae6
Dan <105918404+DenysKors@users.noreply.github.com>
2026-02-20 13:08:33 +0200

    Undo webhook

--- scripts/build.ts
+++
@@ -1,7 +0,0 @@
-import { bot } from "@/app/bot";
-
-const webhook: string = process.env.WEBHOOK || "";
-
-const webhookUrl = `${webhook}api/webhook`;
-
-void bot.api.setWebhook(webhookUrl);
--- services/telegram.ts
+++ services/telegram.ts
@@ -1,9 +1,5 @@
-// import { Bot } from "grammy";
+import { Bot } from "grammy";
 
 const token: string = process.env.BOT_TOKEN || "";
 
-// export const bot = new Bot(token);
-
-// bot.on("message", async (ctx) => {
-//   await ctx.reply("Hi there!");
-// });
+export const bot = new Bot(token);
