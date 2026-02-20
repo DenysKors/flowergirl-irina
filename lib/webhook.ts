95c66b261530d92b55c0a0a614197e0068e1cd95
Dan <105918404+DenysKors@users.noreply.github.com>
2026-02-20 11:31:11 +0200

    Fix build error

--- lib/webhook.ts
+++ lib/webhook.ts
@@ -1,4 +1,4 @@
-import { bot } from "@/bot";
+import { bot } from "@/app/bot";
 import { webhookCallback } from "grammy";
 
 // webhookCallback will make sure that the correct middleware(listener) function is called
